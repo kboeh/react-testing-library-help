cfa521f5a975ee61cd41b386bb7a9403c75d4012
KBoeh <kboeh3@gmail.com>
2022-10-19 19:06:31 -0400

    removed comments from test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,3 @@
-/*
-Hi! Need help with React Testing Library? The best way to get it is by forking
-this repository, making a reproduction of your issue (or showing what you're
-trying to do), and posting a link to your fork on our Discord chat:
-
-https://testing-library.com/discord
-*/
-
-// here's an example
 import React from 'react'
 import '@testing-library/jest-dom'
 import {render, screen, fireEvent} from '@testing-library/react'
